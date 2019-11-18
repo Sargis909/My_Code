a258bb9584fa7bccd56a155f1a2e9b3bbe814028
Sargis909 <57678239+Sargis909@users.noreply.github.com>
2019-11-18 14:03:19 +0400

    Update fibonacci_generator.py

--- fibonacci_generator.py
+++ fibonacci_generator.py
@@ -1,11 +1,11 @@
 def fibonacci_generator():
-    a = b = 1
+    a = b = 1  #Start number
     while True:
         yield a
         a , b = b , a + b 
 fib = fibonacci_generator()
 for i in fib:
-    if i > 100:
+    if i > 100: #End number
         break
     else:
         print('Generated: ' , i )
